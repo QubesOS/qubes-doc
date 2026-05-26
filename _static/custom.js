f2535300d6a8fc28c01d3bd7ec4b991ba3c78446
unman <unman@thirdeyesecurity.org>
2026-05-26 14:03:43 +0000

    Absent any feedback I have decided to flag external guides as dangerous. This is open to review

--- _static/custom.js
+++ _static/custom.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
       const warningDiv = document.createElement('div');
       warningDiv.classList.add('rst-content')
       warningDiv.innerHTML = `
-        <div class="admonition warning">
-            <p class="admonition-title">Warning</p>
+        <div class="admonition danger">
+            <p class="admonition-title">Danger</p>
             <p>This is unofficial content provided by community members or third parties. These guides are not maintained by the Qubes team. Use them at your own risk.</p>
         </div>
       `;
